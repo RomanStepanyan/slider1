9a5066615487d34fd7bb4a718758c6e83f6912d8
RomanStepanyan <stepik10@gmail.com>
2020-06-21 02:12:30 +0300

    Not responcive slider

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider',
+    centerMode: true,
+    focusOnSelect: true,        
+    responcive: [
+      {
+        breakpoint: 768, 
+        settings: {
+          slidesToShow: 4
+        }
+      }
+    ],
+    mobileFirst: true,
+});
+});
+
